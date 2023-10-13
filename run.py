3329ddd724608215d6ea85a70c2fd3100b3eea99
Nicolas-Vinckier <nicolas.vinckier05@gmail.com>
2023-10-13 16:20:01 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,15 +1,6 @@
 import os
 import time
 
-
-def get_date():
-    return time.strftime("%Y-%m-%d", time.gmtime())
-
-
-def get_time():
-    return time.strftime("%H-%M-%S", time.gmtime())
-
-
 old_path = "ancienne_version"
 new_path = "nouvelle_version"
 
@@ -22,59 +13,35 @@ def create_folder():
         os.mkdir(new_path)
         print(f"Création du dossier {new_path}")
     else:
+        # print("Les dossiers existent déjà")
         pass
 
 
-# Fonction qui permet de vérifier si il y a des nouveau fichiers dans le dossier new_path
-def check_new_files():
-    new_files = []
-    for file in os.listdir(new_path):
-        if file not in os.listdir(old_path):
-            new_files.append(f"Le fichier {new_path}/{file} a été ajouté")
-    return new_files
+# Fonction récursive pour vérifier les fichiers et dossiers
+def check_directory(old_dir, new_dir):
+    for root, dirs, files in os.walk(new_dir):
+        for file in files:
+            old_file_path = os.path.join(old_dir, os.path.relpath(root, new_dir), file)
+            new_file_path = os.path.join(new_dir, os.path.relpath(root, new_dir), file)
 
+            if not os.path.exists(old_file_path):
+                print(f"Le fichier {new_file_path} a été ajouté")
+            elif os.path.getmtime(new_file_path) > os.path.getmtime(old_file_path):
+                print(f"Le fichier {new_file_path} a été modifié")
 
-# Fonction qui permet de vérifier si il y a des fichiers modifiés dans le dossier new_path
-def check_modified_files():
-    modified_files = []
-    for file in os.listdir(new_path):
-        if file in os.listdir(old_path):
-            if os.path.getmtime(f"{new_path}/{file}") > os.path.getmtime(
-                f"{old_path}/{file}"
-            ):
-                modified_files.append(f"Le fichier {new_path}/{file} a été modifié")
-    return modified_files
+        for dir in dirs:
+            old_sub_dir = os.path.join(old_dir, os.path.relpath(root, new_dir), dir)
+            new_sub_dir = os.path.join(new_dir, os.path.relpath(root, new_dir), dir)
 
-
-# Fonction qui permet de vérifier si il y a des fichiers supprimés dans le dossier new_path
-def check_deleted_files():
-    deleted_files = []
-    for file in os.listdir(old_path):
-        if file not in os.listdir(new_path):
-            deleted_files.append(f"Le fichier {old_path}/{file} a été supprimé")
-    return deleted_files
+            if not os.path.exists(old_sub_dir):
+                print(f"Le dossier {new_sub_dir} a été ajouté")
+            else:
+                check_directory(old_sub_dir, new_sub_dir)
 
 
 def main():
     create_folder()
-    new_files = check_new_files()
-    modified_files = check_modified_files()
-    deleted_files = check_deleted_files()
-
-    if new_files:
-        print("Nouveaux fichiers/dossiers détectés:")
-        for file in new_files:
-            print(file)
-
-    if modified_files:
-        print("Fichiers/dossiers modifiés détectés:")
-        for file in modified_files:
-            print(file)
-
-    if deleted_files:
-        print("Fichiers/dossiers supprimés détectés:")
-        for file in deleted_files:
-            print(file)
+    check_directory(old_path, new_path)
 
 
 if __name__ == "__main__":
