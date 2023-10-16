0e0b5a839ae1fc5ce625e06b297cf4af014c40fe
Dev-EtapesPrint <dev@etapes-print.com>
2023-10-16 09:47:48 +0200

    &manuel

--- run.py
+++ run.py
@@ -1,14 +1,14 @@
 import os
-import datetime
 import shutil
 
 old_path = "ancienne_version"
 new_path = "nouvelle_version"
 refacto_folder = "refacto_version"
+user_modifications_folder = "modifications_utilisateur"
 
 
 def create_folder():
-    for path in [old_path, new_path, refacto_folder]:
+    for path in [old_path, new_path, refacto_folder, user_modifications_folder]:
         if not os.path.exists(path):
             os.mkdir(path)
             print(f"Création du dossier {path}")
@@ -30,14 +30,23 @@ def sync_and_refactor(old_path, new_path, refacto_path):
         old_item_path = os.path.join(old_path, item)
         new_item_path = os.path.join(new_path, item)
         refacto_item_path = os.path.join(refacto_path, item)
+        user_modified_path = os.path.join(user_modifications_folder, item)
 
         if os.path.isfile(new_item_path):
-            if not os.path.exists(old_item_path) or os.path.getmtime(
+            # Si le fichier a été modifié par l'utilisateur, utiliser cette version
+            if os.path.exists(user_modified_path):
+                shutil.copy2(user_modified_path, refacto_item_path)
+                print(
+                    f"Copie des modifications de l'utilisateur de {user_modified_path} vers {refacto_item_path}"
+                )
+            # Sinon, utiliser la version de nouvelle_version
+            elif not os.path.exists(old_item_path) or os.path.getmtime(
                 old_item_path
             ) != os.path.getmtime(new_item_path):
                 shutil.copy2(new_item_path, refacto_item_path)
                 print(f"Copie de {new_item_path} vers {refacto_item_path}")
 
+        # Si c'est un dossier, descendre récursivement
         elif os.path.isdir(new_item_path):
             if not os.path.exists(refacto_item_path):
                 os.mkdir(refacto_item_path)
