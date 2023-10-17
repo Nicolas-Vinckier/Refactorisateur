33311a6a7de7df29f5b28f17aeda32075a48d2da
Nicolas-Vinckier <nicolas.vinckier05@gmail.com>
2023-10-17 13:56:02 +0200

    Update comparateur.py

--- comparateur.py
+++ comparateur.py
@@ -1,7 +1,7 @@
-import os 
+import os
 
 def compare_files(file1_path, file2_path):
-    with open(file1_path, 'r') as file1, open(file2_path, 'r') as file2:
+    with open(file1_path, "r") as file1, open(file2_path, "r") as file2:
         file1_lines = file1.read().splitlines()
         file2_lines = file2.read().splitlines()
         for i, (line1, line2) in enumerate(zip(file1_lines, file2_lines), start=1):
@@ -11,6 +11,15 @@ def compare_files(file1_path, file2_path):
                 print(f"{file2_path}: {line2}")
         print("Comparison complete.")
 
-# Example usage:
-compare_files("file1.txt", "file2.txt")
+# Ask user for file paths
+file1_path = input("Enter path for first file: ")
+file2_path = input("Enter path for second file: ")
 
+# Check if files exist
+if not os.path.isfile(file1_path):
+    print(f"{file1_path} does not exist.")
+elif not os.path.isfile(file2_path):
+    print(f"{file2_path} does not exist.")
+else:
+    # Compare files
+    compare_files(file1_path, file2_path)
