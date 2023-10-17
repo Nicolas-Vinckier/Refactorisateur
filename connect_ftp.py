84ef27d96d90ce6768f530a2356e58816911bbaa
Dev-EtapesPrint <dev@etapes-print.com>
2023-10-17 16:20:45 +0200

    Update connect_ftp.py

--- connect_ftp.py
+++ connect_ftp.py
@@ -1,10 +1,12 @@
 import paramiko
 import os
+import time
 
-# === Dossier local
-dossier_client = os.path.abspath("serveur_sftp")
+# === Dossier local racine
+local_folder = os.path.abspath("serveur_sftp")
 
-os.makedirs(dossier_client, exist_ok=True)
+# Assurez-vous que le dossier local existe
+os.makedirs(local_folder, exist_ok=True)
 os.makedirs("PasswordFolder", exist_ok=True)
 
 # === Chemin parent sur le serveur distant
@@ -16,9 +18,6 @@ def clear_screen():
 
 
 def list_files_on_sftp_folder(sftp, path):
-    """
-    Liste les dossiers et fichiers du serveur SFTP et les renvoie sous forme de deux listes.
-    """
     files_list = []
     folders_list = []
     try:
@@ -34,7 +33,6 @@ def list_files_on_sftp_folder(sftp, path):
 
 
 def sftp_join(*args):
-    """Une version simple de os.path.join pour les chemins SFTP."""
     return "/".join(arg.strip("/") for arg in args)
 
 
@@ -49,17 +47,17 @@ def connect_to_sftp(host, port, username, password):
         return None
 
 
-def download_recursive(sftp, remote_path, local_path):
+def download_recursive(sftp, remote_path, local_root):
+    local_path = os.path.join(local_root, remote_path.lstrip("/"))
+    if not os.path.exists(local_path):
+        os.makedirs(local_path)
+
     # Vérification d'existence du chemin distant
     try:
         sftp.stat(remote_path)
     except FileNotFoundError:
         print(f"Chemin distant introuvable: {remote_path}")
         return
-
-    # Créer le dossier local si ce n'est pas déjà fait
-    if not os.path.exists(local_path):
-        os.makedirs(local_path)
 
     # Lister les fichiers/dossiers à cet emplacement
     files, folders = list_files_on_sftp_folder(sftp, remote_path)
@@ -77,47 +75,35 @@ def download_recursive(sftp, remote_path, local_path):
     for folder in folders:
         remote_folder_path = sftp_join(remote_path, folder)
         local_folder_path = os.path.join(local_path, folder)
-        try:
-            download_recursive(sftp, remote_folder_path, local_folder_path)
-        except Exception as e:
-            print(f"Erreur lors de la navigation dans {remote_folder_path}: {e}")
+        download_recursive(sftp, remote_folder_path, local_folder_path)
 
 
 def main():
-    clear_screen()
+    while True:
+        clear_screen()
+        host = "ftp.cluster028.hosting.ovh.net"
+        port = 22
+        username = "myvcarh"
 
-    # Informations de connexion SFTP
-    host = "ftp.cluster028.hosting.ovh.net"
-    port = 22
-    username = "myvcarh"
+        # Lire le mot de passe depuis le fichier 'myvcardpro.txt'
+        with open("PasswordFolder/myvcardpro.txt", "r") as f:
+            password = f.read().strip()
 
-    # Lire le mot de passe depuis le fichier 'myvcardpro.txt'
-    with open("PasswordFolder/myvcardpro.txt", "r") as f:
-        password = f.read().strip()
+        sftp = connect_to_sftp(host, port, username, password)
+        if not sftp:
+            print("Impossible de se connecter au serveur SFTP.")
+            return
 
-    sftp = connect_to_sftp(host, port, username, password)
-    if not sftp:
-        print("Impossible de se connecter au serveur SFTP.")
-        return
+        print(
+            f"Connexion au serveur SFTP {host}:{port} avec l'utilisateur {username}.\n"
+        )
+        download_recursive(sftp, parent_folder, local_folder)
 
-    print(f"---------------------------- Connexion SFTP ----------------------------")
-    print(f"Connexion au serveur SFTP {host}:{port} avec l'utilisateur {username}.\n")
+        # Fermer la connexion SFTP
+        sftp.close()
 
-    download_recursive(sftp, parent_folder, dossier_client)
-
-    print(
-        f"---------------------------- Dossier distant: {parent_folder} ----------------------------"
-    )
-    files_list, folders_list = list_files_on_sftp_folder(sftp, parent_folder)
-
-    print("Fichiers:")
-    print(files_list)
-
-    print("Dossiers:")
-    print(folders_list)
-
-    # Fermer la connexion SFTP
-    sftp.close()
+        # Attendez 10 minutes avant de resynchroniser
+        time.sleep(600)
 
 
 if __name__ == "__main__":
