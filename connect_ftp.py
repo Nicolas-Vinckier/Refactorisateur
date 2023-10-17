e06295a1160253bb2189e977bfd5d927d9860c1d
Dev-EtapesPrint <dev@etapes-print.com>
2023-10-17 16:07:29 +0200

    Update connect_ftp.py

--- connect_ftp.py
+++ connect_ftp.py
@@ -15,35 +15,6 @@ def clear_screen():
     os.system("cls" if os.name == "nt" else "clear")
 
 
-def compare_files_folders(local_files, local_folders, sftp_files, sftp_folders):
-    missing_files = [f for f in sftp_files if f not in local_files]
-    missing_folders = [f for f in sftp_folders if f not in local_folders]
-
-    # Ici, nous supposons que vous avez toujours la version la plus récente des fichiers.
-    # Vous pouvez ajouter une vérification des dates de modification si nécessaire.
-    updated_files = local_files
-
-    return missing_files, missing_folders, updated_files
-
-
-def download_missing_items(sftp, missing_files, missing_folders):
-    for folder in missing_folders:
-        local_path = os.path.join(dossier_client, folder)
-        os.makedirs(local_path, exist_ok=True)
-
-    for file in missing_files:
-        local_path = os.path.join(dossier_client, file)
-        remote_path = os.path.join(parent_folder, file)
-        sftp.get(remote_path, local_path)
-
-
-def upload_updated_files(sftp, updated_files):
-    for file in updated_files:
-        local_path = os.path.join(dossier_client, file)
-        remote_path = os.path.join(parent_folder, file)
-        sftp.put(local_path, remote_path)
-
-
 def list_files_on_sftp_folder(sftp, path):
     """
     Liste les dossiers et fichiers du serveur SFTP et les renvoie sous forme de deux listes.
@@ -62,6 +33,11 @@ def list_files_on_sftp_folder(sftp, path):
     return files_list, folders_list
 
 
+def sftp_join(*args):
+    """Une version simple de os.path.join pour les chemins SFTP."""
+    return "/".join(arg.strip("/") for arg in args)
+
+
 def connect_to_sftp(host, port, username, password):
     try:
         transport = paramiko.Transport((host, port))
@@ -71,6 +47,40 @@ def connect_to_sftp(host, port, username, password):
     except Exception as e:
         print(f"Erreur de connexion SFTP : {e}")
         return None
+
+
+def download_recursive(sftp, remote_path, local_path):
+    # Vérification d'existence du chemin distant
+    try:
+        sftp.stat(remote_path)
+    except FileNotFoundError:
+        print(f"Chemin distant introuvable: {remote_path}")
+        return
+
+    # Créer le dossier local si ce n'est pas déjà fait
+    if not os.path.exists(local_path):
+        os.makedirs(local_path)
+
+    # Lister les fichiers/dossiers à cet emplacement
+    files, folders = list_files_on_sftp_folder(sftp, remote_path)
+
+    # Télécharger chaque fichier
+    for file in files:
+        remote_file_path = sftp_join(remote_path, file)
+        local_file_path = os.path.join(local_path, file)
+        try:
+            sftp.get(remote_file_path, local_file_path)
+        except Exception as e:
+            print(f"Erreur lors du téléchargement de {remote_file_path}: {e}")
+
+    # Récursion dans chaque sous-dossier
+    for folder in folders:
+        remote_folder_path = sftp_join(remote_path, folder)
+        local_folder_path = os.path.join(local_path, folder)
+        try:
+            download_recursive(sftp, remote_folder_path, local_folder_path)
+        except Exception as e:
+            print(f"Erreur lors de la navigation dans {remote_folder_path}: {e}")
 
 
 def main():
@@ -86,44 +96,28 @@ def main():
         password = f.read().strip()
 
     sftp = connect_to_sftp(host, port, username, password)
+    if not sftp:
+        print("Impossible de se connecter au serveur SFTP.")
+        return
 
     print(f"---------------------------- Connexion SFTP ----------------------------")
     print(f"Connexion au serveur SFTP {host}:{port} avec l'utilisateur {username}.\n")
 
-    if sftp:
-        print(
-            f"---------------------------- Dossier distant: {parent_folder} ----------------------------"
-        )
-        files_list, folders_list = list_files_on_sftp_folder(sftp, parent_folder)
+    download_recursive(sftp, parent_folder, dossier_client)
 
-        print("Fichiers:")
-        print(files_list)
+    print(
+        f"---------------------------- Dossier distant: {parent_folder} ----------------------------"
+    )
+    files_list, folders_list = list_files_on_sftp_folder(sftp, parent_folder)
 
-        print("Dossiers:")
-        print(folders_list)
+    print("Fichiers:")
+    print(files_list)
 
-        # Récupérer la liste des fichiers et dossiers locaux
-        local_files = os.listdir(dossier_client)
-        local_folders = [
-            f for f in local_files if os.path.isdir(os.path.join(dossier_client, f))
-        ]
-        local_files = [f for f in local_files if f not in local_folders]
+    print("Dossiers:")
+    print(folders_list)
 
-        # Comparer les fichiers et dossiers
-        missing_files, missing_folders, updated_files = compare_files_folders(
-            local_files, local_folders, files_list, folders_list
-        )
-
-        # Télécharger les fichiers/dossiers manquants
-        download_missing_items(sftp, missing_files, missing_folders)
-
-        # Envoyer les fichiers mis à jour
-        upload_updated_files(sftp, updated_files)
-
-        # Fermer la connexion SFTP
-        sftp.close()
-    else:
-        print("La connexion SFTP a échoué.")
+    # Fermer la connexion SFTP
+    sftp.close()
 
 
 if __name__ == "__main__":
