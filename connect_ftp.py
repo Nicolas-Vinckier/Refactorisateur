7ae1c022f1e6d723141f8b42b56c9ed3f487c683
Dev-EtapesPrint <dev@etapes-print.com>
2023-10-17 15:56:59 +0200

    sftp

--- connect_ftp.py
+++ connect_ftp.py
@@ -2,7 +2,8 @@ import paramiko
 import os
 
 # === Dossier local
-dossier_client = "serveur_sftp"
+dossier_client = os.path.abspath("serveur_sftp")
+
 os.makedirs(dossier_client, exist_ok=True)
 os.makedirs("PasswordFolder", exist_ok=True)
 
@@ -12,6 +13,35 @@ parent_folder = "/home/myvcarh/www/"
 
 def clear_screen():
     os.system("cls" if os.name == "nt" else "clear")
+
+
+def compare_files_folders(local_files, local_folders, sftp_files, sftp_folders):
+    missing_files = [f for f in sftp_files if f not in local_files]
+    missing_folders = [f for f in sftp_folders if f not in local_folders]
+
+    # Ici, nous supposons que vous avez toujours la version la plus récente des fichiers.
+    # Vous pouvez ajouter une vérification des dates de modification si nécessaire.
+    updated_files = local_files
+
+    return missing_files, missing_folders, updated_files
+
+
+def download_missing_items(sftp, missing_files, missing_folders):
+    for folder in missing_folders:
+        local_path = os.path.join(dossier_client, folder)
+        os.makedirs(local_path, exist_ok=True)
+
+    for file in missing_files:
+        local_path = os.path.join(dossier_client, file)
+        remote_path = os.path.join(parent_folder, file)
+        sftp.get(remote_path, local_path)
+
+
+def upload_updated_files(sftp, updated_files):
+    for file in updated_files:
+        local_path = os.path.join(dossier_client, file)
+        remote_path = os.path.join(parent_folder, file)
+        sftp.put(local_path, remote_path)
 
 
 def list_files_on_sftp_folder(sftp, path):
@@ -71,6 +101,24 @@ def main():
 
         print("Dossiers:")
         print(folders_list)
+
+        # Récupérer la liste des fichiers et dossiers locaux
+        local_files = os.listdir(dossier_client)
+        local_folders = [
+            f for f in local_files if os.path.isdir(os.path.join(dossier_client, f))
+        ]
+        local_files = [f for f in local_files if f not in local_folders]
+
+        # Comparer les fichiers et dossiers
+        missing_files, missing_folders, updated_files = compare_files_folders(
+            local_files, local_folders, files_list, folders_list
+        )
+
+        # Télécharger les fichiers/dossiers manquants
+        download_missing_items(sftp, missing_files, missing_folders)
+
+        # Envoyer les fichiers mis à jour
+        upload_updated_files(sftp, updated_files)
 
         # Fermer la connexion SFTP
         sftp.close()
